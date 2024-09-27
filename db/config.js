445f9eb810355ea3452653cb0d03af77a1716fce
jaydeep078 <jaydeepgurjar6@gmail.com>
2024-09-27 17:04:05 +0530

    2nd commit

--- db/config.js
+++ db/config.js
@@ -1,2 +1,2 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/e-commerce")
+mongoose.connect("mongodb+srv://jaydeepmalvya210189:qrwZH78fu7pMUAj9@cluster0.6etcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/e-commerce")
